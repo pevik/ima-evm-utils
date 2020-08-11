c7b9da44e6bc3a1af8b8b38e5ce25fa0a9074361
Petr Vorel <pvorel@suse.cz>
2020-08-11 17:57:03 +0200

    install-swtpm.sh: Prepare for non-native builds
    Support 32-bit build and cross compilation builds.
    This is a preparation for future commit (moving to docker based Travis CI).
    
    + remove useless cd ../..
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- tests/install-swtpm.sh
+++ tests/install-swtpm.sh
@@ -8,6 +8,29 @@ mkdir ibmtpm$version
 cd ibmtpm$version
 tar -xvzf ../download
 cd src
+
+CC="${CC:-gcc}"
+for i in /usr/*/include/; do
+	CFLAGS="-I$i $CFLAGS"
+done
+
+for i in /usr/*/lib*/; do
+	LDFLAGS="-L$i $LDFLAGS"
+done
+
+if [ "$VARIANT" = "i386" ]; then
+	echo "32-bit compilation"
+	CFLAGS="-m32 $CFLAGS" LDFLAGS="-m32 $LDFLAGS"
+fi
+
+export CC CFLAGS LDFLAGS
+
+echo "=== compiler version ==="
+$CC --version
+echo "CFLAGS: '$CFLAGS'"
+echo "LDFLAGS: '$LDFLAGS'"
+echo "PREFIX: '$PREFIX'"
+
+echo "=== make ==="
 make -j$(nproc)
 sudo cp tpm_server /usr/local/bin/
-cd ../..
